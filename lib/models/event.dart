20ed2c7f312d4550799c878c9d96a689f0a15c50
Novan <novan.allanadi1211@gmail.com>
2019-03-17 03:06:36 +1100

    added event functionality

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,14 +1,23 @@
+import 'profile.dart';
+
 class Event {
   String location, description, title;
   DateTime startTime, endTime;
-  List<String> participants = [];
+  var participants = [];
   var schedule = new Map();
 
   void addParticipants(String participant) => participants.add(participant);
+  void addAdmin(Profile prof) => participants.add({
+        prof.userID: 'admin',
+      });
 
+  void addRegular(Profile prof) => participants.add({
+        prof.userID: 'regular',
+      });
+      
   void addSchedule(String activity, DateTime time) =>
       this.schedule[activity] = time;
-      
+
   Map allMapped() {
     Map map = {
       "Description": description,
